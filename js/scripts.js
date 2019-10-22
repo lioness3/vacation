d5dddf7553204ec0e113b5ae08bdf8cedf5fd5e7
Student <example@example.com>
2019-10-22 11:24:54 -0700

    add buttons

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+$(function() {
+  $("button#pink").click(function(){
+    $("body").removeClass();
+    $("body").addClass("pink");
+  });
+  $("button#blue").click(function(){
+    $("body").removeClass();
+    $("body").addClass("blue");
+  });
+
+
+});
